f75011ad8c5c11f5ab8302ed6617e1ac67f47782
horvathaa <horvatha@onid.oregonstate.edu>
2016-10-05 15:58:44 -0700

    starting assignment 1

---
+++ gp1.py
@@ -0,0 +1,9 @@
+'''Suppose you are given two sets of n points, one set {p1, p2, . . . , pn} on the line y = 0 and the
+other set {q1, q2, . . . , qn} on the line y = 1. Suppose, p1 · x < p2 · x < . . . < pn · x (where pi · x
+is the X-coordinate of pi). Create a set of n line segments by connecting each point pi to the
+corresponding point qi. Suppose no three line segments intersect in a point.'''
+
+def createArray:
+
+
+def recurse(p1, q1):
